80aedfa194aa97c1587184bfc01ceb5461e08c71
shaikhfaizan <faizanshaikh2002320@gmail.com>
2023-01-22 17:27:42 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function textscrooling(){
+    if(document.body.scrollTop>100 || document.documentElement.scrollTop>100){
+        document.getElementById("text-zoom").style.fontSize = "200px";
+        document.body.style.color = "red";
+    }else{
+        document.getElementById("text-zoom").style.fontSize = "100px";
+        document.body.style.backgroundColor = "aqua";
+    }
+}
+window.onscroll = function(){
+    textscrooling();
+}
+
+
+
+// document.body.scrollTop is intially is been 0 and document.documentElement.scrollTop is like it also zero but when 
+// we scroll it giving pixel in this condition if the pixel is above 100 then its become true and condition satified
